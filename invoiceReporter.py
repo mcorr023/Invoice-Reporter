c1d5fa1ed5303c32be89bdde772351c34662ec61
Maria Correa <mcorr023@fiu.edu>
2022-09-08 01:46:39 -0400

    Finished based function

--- invoiceReporter.py
+++ invoiceReporter.py
@@ -1,33 +1,35 @@
+import numpy as np
 
 def report(arr1, arr2):
     #sort report and invoices
-    arr1.sort()
-    arr2.sort()
+    np.sort(arr1)
+    np.sort(arr2)
 
-    completed= []
-    duplicates = []
+    completed = np.array([])
+    duplicates = np.array([])
 
     for i in range(len(arr2)):
         if arr2[i] == arr2[i-1]:
-            duplicates.append(arr2[i])
+            duplicates = np.append(duplicates,arr2[i])
 
-    arr2 = list(set(arr2))
+    arr1 = set(arr1)
+    arr2 = set(arr2)
 
-    missing = list(arr1-arr2)
-    extras = list(arr2-arr1) 
+    missing = list(sorted(arr1 - arr2))
+    extras = list(sorted(arr2 - arr1))
     
     for i in arr1:
         for j in arr2:
             if i == j :
-                completed.append(i)
+                completed = np.append(completed, i)
 
     print("Completed: ", completed)
-    print("Missing: ", missing)
     print("Duplicates: ",  duplicates)
     print("Extras: ", extras)
+    print("Missing: ", missing)
 
-test1 = ["111","222","333","444","555","666","777"]
-test2 = ["111","222","222","444", "888"]
+test1 = np.array(['111','222','333','444','555','666','777'])
+test2 = np.array(['111','222','222','444', '888'])
 
 report(test1,test2)
 
