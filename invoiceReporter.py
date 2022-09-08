8bb7064170ac9b30268711bab7e13a4ab04501fd
Maria Correa <mcorr023@fiu.edu>
2022-09-08 01:07:02 -0400

    Fixed issues

--- invoiceReporter.py
+++ invoiceReporter.py
@@ -1,31 +1,33 @@
 
-
 def report(arr1, arr2):
     #sort report and invoices
     arr1.sort()
-    arr2.sort(key=lambda x: x[1])
+    arr2.sort()
 
-    missing = []
-    completed, duplicate, extra = [[]]
+    completed= []
+    duplicates = []
 
-    for i in range (len(arr1)-1,0,-1):
-        for j in range (len(arr2)-1,0,-1):
-            if arr2[j,:] == arr1[i]:
-                completed.append(arr2[j,:])
-            if arr1[i] not in arr2[j,:]:
-                missing.append(arr1[i])
-            if arr2[j,:] not in arr1[i]:
-                extra.append(arr2[j,:])
-            if arr2[j,:] == arr2[j-1,:]:
-                duplicate.append(arr2[j,:])
+    for i in range(len(arr2)):
+        if arr2[i] == arr2[i-1]:
+            duplicates.append(arr2[i])
 
-    print("Completed: " + completed)
-    print("Missing: " + missing)
-    print("Duplicates: " + duplicate)
-    print("Extras: " + extra)
+    arr2 = list(set(arr2))
 
-test1 = [111,222,333,444,555,666,777]
-test2 = [[111,2],[222,3],[222,4],[444,5]]
+    missing = list(arr1-arr2)
+    extras = list(arr2-arr1) 
+    
+    for i in arr1:
+        for j in arr2:
+            if i == j :
+                completed.append(i)
+
+    print("Completed: ", completed)
+    print("Missing: ", missing)
+    print("Duplicates: ",  duplicates)
+    print("Extras: ", extras)
+
+test1 = ["111","222","333","444","555","666","777"]
+test2 = ["111","222","222","444", "888"]
 
 report(test1,test2)
 
