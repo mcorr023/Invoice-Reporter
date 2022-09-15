4d6d003384549e80780cca3ea3b1ef80d6c8db84
Maria Correa <mcorr023@fiu.edu>
2022-09-15 14:09:24 -0400

    Start making GUI

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import invoiceReporter
 import pdfConverter
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput 
 
-invoiceReporter.report(pdfConverter.importReport(), pdfConverter.importInvoices())
+class InvoiceReportApp(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 2
+        self.window.add_widget(Image("icon.png"))
+
+        return self.window
+
+if __name__ == "__main__":
+    InvoiceReportApp().run()
+
+
+#invoiceReporter.report(pdfConverter.importReport(), pdfConverter.importInvoices())
